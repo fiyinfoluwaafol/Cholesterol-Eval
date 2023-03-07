7a9e542b6626a0d74af78fa899f3e007f7be732e
fiyinfoluwaafol <fiyinfoluwa.afolayan@gmail.com>
2023-03-07 09:07:59 -0500

    Program can successfully read data for at least the first patient

--- Cholestrol_Aid.cpp
+++ Cholestrol_Aid.cpp
@@ -7,33 +7,39 @@ ifstream inputFile("infile.txt");
 string line, line1;
 int main()
 {
-    int num_of_patients; 
-    char num_of_sets;
-    cin >> num_of_patients;
-        if (inputFile.is_open())
-        {
-            while (getline(inputFile,line))
-            {
-                
-                if (isalpha(line[0]))
-                {
-                    num_of_sets = line.at((line.length()-1));
-                    //cout << num_of_sets << endl;
-                    for (int i = 1; i <= num_of_sets; i++)
-                    {
-                        getline(inputFile,line1);
-                        if (isalpha(line1[0]))
-                        {
-                            cout << "BREAKING POINT" << endl;
-                            continue;
-                        }
-                        cout << line1 << endl;
-                    }
-                }
-                
-            }
-        }
-        inputFile.close();
+    int num_of_patients;
+    string patient_name;
+    string HDL,LDL,systolic,diastolic;
+    string num_of_sets;
 
+    cin >> num_of_patients;
+    if (inputFile)
+    {
+        //for (int i = 0; i < num_of_patients; i++)
+        //{
+            getline(inputFile,patient_name,' ');
+            inputFile.ignore(2,' ');
+            getline(inputFile,num_of_sets);
+            cout << "The patient's name is " << patient_name << endl;
+            cout << "The patient has " << num_of_sets << " sets of readings" << endl;
+            for (int j = 1; j <= stoi(num_of_sets); j++)
+            {
+                getline(inputFile,HDL,' ');
+                getline(inputFile,LDL,' ');
+                getline(inputFile,systolic,' ');
+                getline(inputFile,diastolic,'\n');
+
+                cout << "Data Set " << j << endl;
+                cout << "This patient's HDL is " << HDL << endl;
+                cout << "This patient's LDL is " << LDL << endl;
+                cout << "This patient's systolic is " << systolic << endl;
+                cout << "This patient's diastolic is " << diastolic << endl;
+                cout << "\n";
+            }
+        //}
+    }
+        
+       // }
+    inputFile.close();
     return 0;
 }
