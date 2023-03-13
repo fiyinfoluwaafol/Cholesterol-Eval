00dddce96a8e506b9fedf5a4271e46c62591590e
csprojexCA <125930012+csprojexCA@users.noreply.github.com>
2023-03-13 17:26:12 -0400

    Update Cholestrol_Aid.cpp
    Put in some stuff for evaluate_cholesterol function

--- Cholestrol_Aid.cpp
+++ Cholestrol_Aid.cpp
@@ -99,8 +99,52 @@ float HDL_LDL_ratio(int HLevels, int Llevels)
  */
 void evaluate_cholesterol(int HLevels, int Llevels, string& string_HDL, string& string_LDL)
 {
-    cout << "This patient's HDL is " << HLevels << endl;
+    cout << "  HDL: " + string_HDL + " LDL: " + string_LDL << endl; //printing to the screen the patient's HDL and LDL levels using the string pointers
+    cout << "This patient's HDL is " << HLevels << endl; 
     cout << "This patient's LDL is " << Llevels << endl;
+    
+  if (HDL < 1)
+  {
+    cout << "very low - Do you want to double-check the data?" << endl; //checking if patient data was wrong (unexpected number)
+  }  
+  else if (HDL < 40) 
+  {  
+    cout << "HDL is too low" << endl; //HDL is too low if it is below 40
+  }
+  else if (HDL < 60)
+  {
+    cout << "HDL is Okay" << endl; //HDL is okay if it is below 60
+  }
+  else 
+  {
+    cout << "HDL is Excellent" << endl; //HDL is excellent if it is at or above 60
+  }
+    
+   
+   
+  
+  if (LDL < 1){
+    cout << "very low - Do you want to double-check the data?" << endl;  //checking if patient data was wrong (unexpected number)
+  }  
+  else if (LDL < 100 ) {  
+    cout << "LDL is Optimal" << endl; //LDL is opitmal if it is below 100
+  }
+  else if (LDL < 130) {
+    cout << "LDL is Near Optimal" << endl; //LDL is near optimal if it is below 130
+  }
+  else if (LDL < 160) {
+    cout << "LDL is Boderline High" << endl; //LDL is borderline high if it is below 160
+  }
+  else if (LDL < 190) {
+    cout << "LDL is High" << endl; //LDL is high if it is below 190
+  }
+  else {
+    cout << "LDL is Very High" << endl; //LDL is very high if it is at or above 190
+  }
+    
+    
+    
+    
 }
 
 /**
